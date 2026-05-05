447c95f05c6fdbc817778467c6b8b2ae88dda55e
Dominik Henneke <dominik.henneke@tentaclelabs.com>
2026-05-05 22:25:37 +0200

    Add documentation

--- plg_sppagebuilder_ytgdpr/addons/youtube_embedder/admin.php
+++ plg_sppagebuilder_ytgdpr/addons/youtube_embedder/admin.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * @package     Joomla.Plugin.Sppagebuilder.YoutubeGdpr
+ * @subpackage  Sppagebuilder.YoutubeGdpr
+ *
+ * @copyright   (C) 2026 Dominik Henneke
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
--- plg_sppagebuilder_ytgdpr/youtube_gdpr.php
+++ plg_sppagebuilder_ytgdpr/youtube_gdpr.php
@@ -1,7 +1,11 @@
 <?php
 
 /**
- * Custom YouTube Plugin for GDPR-compliant rendering.
+ * @package     Joomla.Plugin.Sppagebuilder.YoutubeGdpr
+ * @subpackage  Sppagebuilder.YoutubeGdpr
+ *
+ * @copyright   (C) 2026 Dominik Henneke
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 defined('_JEXEC') or die;
